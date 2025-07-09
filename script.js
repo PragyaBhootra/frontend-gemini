00fbfd0140f5b3650ed0de9cd1c2d376d24c6508
PragyaBhootra <Pbhootra2005@gmail.com>
2025-07-09 18:20:15 +0530

    Update script.js

--- script.js
+++ script.js
@@ -2,7 +2,7 @@ let ws;
 let audioContext, processor, source;
 
 document.getElementById("start").onclick = async () => {
-  ws = new WebSocket("wss://your-backend.onrender.com/ws"); // Replace this
+  ws = new WebSocket("wss://gemini-oy82.onrender.com/ws"); // Replace this
   ws.binaryType = "arraybuffer";
 
   const status = document.getElementById("status");
